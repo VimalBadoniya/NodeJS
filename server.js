10402a5ab179aa42b15125ef0b65b44e1cba0bcd
Vimal-Badoniya <vimal.badoniya.05@gmail.com>
2023-08-15 20:57:21 +0530

    added closing of server using process.exit()

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const myServer = http.createServer((req, res) => {
   res.setHeader('Content-Type' , 'text/html');
   res.write('<html><body><h2>This is first response message for a server</h2></body></html>')
   res.end()
+  process.exit()
   
 });
 myServer.listen(4000);
