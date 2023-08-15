0bda7b777a1f9ba49b1c79a0f83f4b1bcb7c2085
Vimal-Badoniya <vimal.badoniya.05@gmail.com>
2023-08-15 20:41:31 +0530

    created server using http module and createServer() , created response using setHeader() write() and end()

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const myServer = http.createServer((req, res) => {
+  console.log(req.url)
+  res.setHeader('Content-Type' , 'text/html');
+  res.write('<html><body><h2>This is first response message for a server</h2></body></html>')
+  res.end()
+  
+});
+myServer.listen(4000);
+
