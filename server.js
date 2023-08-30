daa960c8c944091f3d507dd3e674b7e77364ae3a
Vimal-Badoniya <vimal.badoniya.05@gmail.com>
2023-08-30 15:49:09 +0530

    created a server and a form in it

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const http = require("http");
 const myServer = http.createServer((req, res) => {
-  console.log(req.url)
+  console.log(req.url , req.method)
   res.setHeader('Content-Type' , 'text/html');
   res.write('<html><body><h2>This is first response message for a server</h2></body></html>')
   res.end()
-  process.exit()
-  
 });
 myServer.listen(4000);
 
